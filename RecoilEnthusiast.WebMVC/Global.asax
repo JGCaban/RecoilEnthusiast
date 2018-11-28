61ecead79eca19331500116c57362d6f57bc7c18
JGCaban <jose.caban317@gmail.com>
2018-11-28 11:29:19 -0500

    Add project files.

---
+++ RecoilEnthusiast.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RecoilEnthusiast.WebMVC.MvcApplication" Language="C#" %>
---
+++ RecoilEnthusiast.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecoilEnthusiast.WebMVC.Startup))]
+namespace RecoilEnthusiast.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
